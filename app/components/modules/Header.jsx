69194b1004945eb21a3afe46eaefc4db5352b5e5
valzav <i@valzav.com>
2016-08-28 17:36:05 -0400

    add horizontal menu; add subheader; hide it on scroll down

--- app/components/modules/Header.jsx
+++ app/components/modules/Header.jsx
@@ -6,6 +6,7 @@ import Icon from 'app/components/elements/Icon.jsx';
 import resolveRoute from 'app/ResolveRoute';
 import DropdownMenu from 'app/components/elements/DropdownMenu';
 import shouldComponentUpdate from 'app/utils/shouldComponentUpdate';
+import HorizontalMenu from 'app/components/elements/HorizontalMenu';
 
 class Header extends React.Component {
     static propTypes = {
@@ -14,7 +15,9 @@ class Header extends React.Component {
 
     constructor() {
         super();
+        this.state = {subheader_hidden: false}
         this.shouldComponentUpdate = shouldComponentUpdate(this, 'Header');
+        this.hideSubheader = this.hideSubheader.bind(this);
     }
 
     componentWillReceiveProps(nextPrps) {
@@ -23,6 +26,24 @@ class Header extends React.Component {
             const sort_order = route && route.page === 'PostsIndex' && route.params && route.params.length > 0 ? route.params[0] : null;
             if (sort_order) window.last_sort_order = this.last_sort_order = sort_order;
         }
+    }
+
+    hideSubheader(){
+        const subheader_hidden = this.state.subheader_hidden;
+        window.scrollY > this.prevScrollY?
+        !subheader_hidden && this.setState({subheader_hidden: true})
+            :
+        subheader_hidden && this.setState({subheader_hidden: false})
+
+        this.prevScrollY = window.scrollY;
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.hideSubheader);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.hideSubheader);
     }
 
     render() {
@@ -67,6 +88,7 @@ class Header extends React.Component {
                 active: 'active',
                 votes: 'popular' };
         const sort_order_menu = Object.keys(sort_orders).filter(so => so !== sort_order).map(so => ({link: `/${so}/${topic}`, value: sort_orders[so]}));
+        const sort_order_menu_horizontal = Object.keys(sort_orders).map(so => ({link: `/${so}/${topic}`, value: sort_orders[so], active: so === sort_order}));
 
         return (
             <header className="Header">
@@ -86,12 +108,23 @@ class Header extends React.Component {
                                 {page_name && <li><span>{page_name}</span></li>}
                                 {sort_order && <li className="delim show-for-medium">|</li>}
                                 {(topic_link || user_name || page_name) && sort_order && <li className="delim show-for-small-only">|</li>}
-                                {sort_order && <DropdownMenu className="Header__sort-order-menu" items={sort_order_menu} selected={sort_orders[sort_order]} el="li" />}
+                                {sort_order && <DropdownMenu className="Header__sort-order-menu show-for-small-only" items={sort_order_menu} selected={sort_orders[sort_order]} el="li" />}
                             </ul>
                         </div>
                         <div className="columns shrink">
                             <TopRightMenu {...this.props} />
                         </div>
+                    </div>
+                </div>
+                <div className={'Header__sub-nav expanded show-for-medium row' + (this.state.subheader_hidden ? ' hidden' : '')}>
+                    <div className="columns">
+                        <HorizontalMenu items={sort_order_menu_horizontal} />
+                    </div>
+                    <div className="columns shrink">
+                        {/*<ul className="menu">
+                            <li><Link to="/market" activeClassName="active">Market</Link></li>
+                            <li><Link to="/users" activeClassName="active">Top Users</Link></li>
+                        </ul>*/}
                     </div>
                 </div>
             </header>
